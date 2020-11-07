32970729b233a27167c0926d722a582bed825b89
asathish02 <asathish02@gmail.com>
2020-11-06 21:12:57 -0500

    Second commit

--- news/settings.py
+++ news/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '+6b_r6*5)adaa8u)9bnbyrjz7!l-7s!w)40+u#^ft3*!xk%b)3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
